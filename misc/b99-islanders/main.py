0d0beede9ef669ee58c118b584c7577439c3de8a
Jeremy Fleischman <jeremyfleischman@gmail.com>
2018-08-16 08:44:41 -0700

    And it's done!

--- misc/b99-islanders/main.py
+++ misc/b99-islanders/main.py
@@ -10,30 +10,52 @@ than the rest?
 
 from enum import Enum
 from dataclasses import dataclass
-from typing import Dict, Set, FrozenSet
+from typing import Dict, FrozenSet, List, Optional, Set
+import functools
+
+import pptree
 
 ######### Types + Helpers ######################
 
 class Weight(Enum):
     """What we know about an islander's weight."""
-    POSSIBLY_HEAVIER = 1
-    POSSIBLY_LIGHTER = 2
+    HEAVIER_OR_NORMAL = 1
+    LIGHTER_OR_NORMAL = 2
     NORMAL = 3
     CLUELESS = 4
 
-@dataclass
+    def to_unicode(self) -> str:
+        """TODO"""
+        if self == Weight.HEAVIER_OR_NORMAL:
+            return "+"
+        elif self == Weight.LIGHTER_OR_NORMAL:
+            return "-"
+        elif self == Weight.NORMAL:
+            return "✔"
+        elif self == Weight.CLUELESS:
+            return "±"
+        else:
+            assert False
+
+@dataclass(frozen=True)
 class Islanders:
     """Represents a collection of islanders."""
-    possibly_heavier_count: int = 0
-    possibly_lighter_count: int = 0
+    heavier_or_normal_count: int = 0
+    lighter_or_normal_count: int = 0
     normal_count: int = 0
     clueless_count: int = 0
 
     def __post_init__(self):
-        assert self.possibly_heavier_count >= 0
-        assert self.possibly_lighter_count >= 0
+        assert self.heavier_or_normal_count >= 0
+        assert self.lighter_or_normal_count >= 0
         assert self.normal_count >= 0
         assert self.clueless_count >= 0
+
+    def is_solved(self):
+        """Returns True if we know which islander weighs a different amount
+        than everyone else."""
+        unknown_count = self.heavier_or_normal_count + self.lighter_or_normal_count + self.clueless_count
+        return unknown_count == 1
 
     def total_count(self):
         """Returns the total number of islanders this object represents."""
@@ -42,24 +64,24 @@ class Islanders:
     def to_count_by_weight(self) -> Dict[Weight, int]:
         """Returns a mapping of Weight to the number of islanders with that Weight."""
         return {
-            Weight.POSSIBLY_HEAVIER: self.possibly_heavier_count,
-            Weight.POSSIBLY_LIGHTER: self.possibly_lighter_count,
+            Weight.HEAVIER_OR_NORMAL: self.heavier_or_normal_count,
+            Weight.LIGHTER_OR_NORMAL: self.lighter_or_normal_count,
             Weight.NORMAL: self.normal_count,
             Weight.CLUELESS: self.clueless_count,
         }
 
     def __add__(self, other):
         return Islanders(
-            possibly_heavier_count=self.possibly_heavier_count + other.possibly_heavier_count,
-            possibly_lighter_count=self.possibly_lighter_count + other.possibly_lighter_count,
+            heavier_or_normal_count=self.heavier_or_normal_count + other.heavier_or_normal_count,
+            lighter_or_normal_count=self.lighter_or_normal_count + other.lighter_or_normal_count,
             normal_count=self.normal_count + other.normal_count,
             clueless_count=self.clueless_count + other.clueless_count,
         )
 
     def __sub__(self, other):
         return Islanders(
-            possibly_heavier_count=self.possibly_heavier_count - other.possibly_heavier_count,
-            possibly_lighter_count=self.possibly_lighter_count - other.possibly_lighter_count,
+            heavier_or_normal_count=self.heavier_or_normal_count - other.heavier_or_normal_count,
+            lighter_or_normal_count=self.lighter_or_normal_count - other.lighter_or_normal_count,
             normal_count=self.normal_count - other.normal_count,
             clueless_count=self.clueless_count - other.clueless_count,
         )
@@ -68,11 +90,11 @@ class Islanders:
         strs = []
         for weight, count in self.to_count_by_weight().items():
             if count > 0:
-                strs.append(f'{count} {weight.name}')
+                strs.append(f'{count}{weight.to_unicode()}')
 
         return "{" + ("Nobody" if len(strs) == 0 else ", ".join(strs)) + "}"
 
-@dataclass
+@dataclass(frozen=True)
 class ScaleOperation:
     """Represents a usage of the scale. On each side of the scale, we place
     some number islanders of each KnownWeight."""
@@ -96,20 +118,13 @@ class ScaleResult(Enum):
     """Which side of the scale tipped down?"""
     LEFT_SIDE = 1
     RIGHT_SIDE = 2
-    SAME = 3
+    BALANCED = 3
 
 class WhereOnScale(Enum):
     """Where an islander can be on a scale."""
     LEFT_SIDE = 1
     RIGHT_SIDE = 2
     NOT_ON_SCALE = 3
-
-#<<< class ScaleObservation(NamedTuple):
-#<<<     operation: ScaleOperation
-#<<<     result: ScaleResult
-#<<<
-#<<<     def __repr__(self) -> str:
-#<<<         return f'{self.operation} leaned {self.result}'
 
 ######################################
 
@@ -122,26 +137,44 @@ def operate_scale(operation: ScaleOperation) -> Dict[ScaleResult, Islanders]:
     right_side = operation.right_side
     not_on_scale: Islanders = operation.get_not_on_scale()
 
-    return {
-        ScaleResult.LEFT_SIDE: Islanders(
-            normal_count=islanders.normal_count + left_side.possibly_lighter_count + right_side.possibly_heavier_count + not_on_scale.possibly_lighter_count + not_on_scale.possibly_heavier_count + not_on_scale.clueless_count,
-            clueless_count=islanders.clueless_count - left_side.clueless_count - right_side.clueless_count - not_on_scale.clueless_count,
-            possibly_heavier_count=islanders.possibly_heavier_count + left_side.clueless_count +  right_side.possibly_heavier_count - not_on_scale.possibly_heavier_count,
-            possibly_lighter_count=islanders.possibly_lighter_count + right_side.clueless_count + left_side.possibly_lighter_count - not_on_scale.possibly_lighter_count,
-        ),
-        ScaleResult.RIGHT_SIDE: Islanders(
-            normal_count=islanders.normal_count + right_side.possibly_lighter_count + left_side.possibly_heavier_count + not_on_scale.possibly_lighter_count + not_on_scale.possibly_heavier_count + not_on_scale.clueless_count,
-            clueless_count=islanders.clueless_count - right_side.clueless_count - left_side.clueless_count - not_on_scale.clueless_count,
-            possibly_heavier_count=islanders.possibly_heavier_count + right_side.clueless_count + left_side.possibly_heavier_count - not_on_scale.possibly_heavier_count,
-            possibly_lighter_count=islanders.possibly_lighter_count + left_side.clueless_count + right_side.possibly_lighter_count - not_on_scale.possibly_lighter_count,
-        ),
-        ScaleResult.SAME: Islanders(
-            normal_count=islanders.normal_count + right_side.possibly_heavier_count + right_side.possibly_lighter_count + right_side.clueless_count + left_side.possibly_heavier_count + left_side.possibly_lighter_count + left_side.clueless_count,
-            clueless_count=islanders.clueless_count - right_side.clueless_count - left_side.clueless_count,
-            possibly_heavier_count=islanders.possibly_heavier_count - right_side.possibly_heavier_count - left_side.possibly_heavier_count,
-            possibly_lighter_count=islanders.possibly_heavier_count - right_side.possibly_lighter_count - left_side.possibly_lighter_count,
-        ),
-    }
+    islanders_by_result = {}
+
+    # Could the scale tip left?
+    heavier_or_normal_count = left_side.heavier_or_normal_count + left_side.clueless_count
+    lighter_or_normal_count = right_side.lighter_or_normal_count + right_side.clueless_count
+    if heavier_or_normal_count + lighter_or_normal_count > 0:
+        islanders_by_result[ScaleResult.LEFT_SIDE] = Islanders(
+            normal_count=islanders.total_count() - heavier_or_normal_count - lighter_or_normal_count,
+            clueless_count=0,
+            heavier_or_normal_count=heavier_or_normal_count,
+            lighter_or_normal_count=lighter_or_normal_count,
+        )
+
+    # Could the scale tip right?
+    heavier_or_normal_count = right_side.heavier_or_normal_count + right_side.clueless_count
+    lighter_or_normal_count = left_side.lighter_or_normal_count + left_side.clueless_count
+    if heavier_or_normal_count + lighter_or_normal_count > 0:
+        islanders_by_result[ScaleResult.RIGHT_SIDE] = Islanders(
+            normal_count=islanders.total_count() - heavier_or_normal_count - lighter_or_normal_count,
+            clueless_count=0,
+            heavier_or_normal_count=heavier_or_normal_count,
+            lighter_or_normal_count=lighter_or_normal_count,
+        )
+
+    # Could the scale balance out?
+    normal_count = not_on_scale.normal_count + left_side.total_count() + right_side.total_count()
+    heavier_or_normal_count = not_on_scale.heavier_or_normal_count
+    lighter_or_normal_count = not_on_scale.lighter_or_normal_count
+    clueless_count = not_on_scale.clueless_count
+    if heavier_or_normal_count + lighter_or_normal_count + clueless_count > 0:
+        islanders_by_result[ScaleResult.BALANCED] = Islanders(
+            normal_count=normal_count,
+            clueless_count=clueless_count,
+            heavier_or_normal_count=heavier_or_normal_count,
+            lighter_or_normal_count=lighter_or_normal_count,
+        )
+
+    return islanders_by_result
 
 def get_operations(islanders: Islanders) -> FrozenSet[ScaleOperation]:
     """
@@ -165,94 +198,67 @@ def get_operations(islanders: Islanders) -> FrozenSet[ScaleOperation]:
     operations: Set[ScaleOperation] = set()
     for left_normal_count in range(islanders.normal_count + 1):
         for clueless_count in range(islanders.clueless_count + 1):
-            for left_heavier_count in range(islanders.possibly_heavier_count + 1):
-                for left_lighter_count in range(islanders.possibly_lighter_count + 1):
+            for left_heavier_count in range(islanders.heavier_or_normal_count + 1):
+                for left_lighter_count in range(islanders.lighter_or_normal_count + 1):
                     left_count = (left_normal_count + clueless_count + left_heavier_count + left_lighter_count)
                     if left_count == 0:
                         continue
                     for right_clueless_count in range(islanders.clueless_count + 1 - clueless_count):
-                        for right_heavier_count in range(islanders.possibly_heavier_count + 1 - left_heavier_count):
+                        for right_heavier_count in range(islanders.heavier_or_normal_count + 1 - left_heavier_count):
                             right_lighter_count = left_count - (right_clueless_count + right_heavier_count)
-                            if 0 <= right_lighter_count <= islanders.possibly_lighter_count - left_lighter_count:
+                            if 0 <= right_lighter_count <= islanders.lighter_or_normal_count - left_lighter_count:
                                 operations.add(ScaleOperation(
                                     all_islanders=islanders,
                                     left_side=Islanders(
                                         normal_count=left_normal_count,
                                         clueless_count=clueless_count,
-                                        possibly_heavier_count=left_heavier_count,
-                                        possibly_lighter_count=left_lighter_count,
+                                        heavier_or_normal_count=left_heavier_count,
+                                        lighter_or_normal_count=left_lighter_count,
                                     ),
                                     right_side=Islanders(
                                         normal_count=0,
                                         clueless_count=right_clueless_count,
-                                        possibly_heavier_count=right_heavier_count,
-                                        possibly_lighter_count=right_lighter_count,
+                                        heavier_or_normal_count=right_heavier_count,
+                                        lighter_or_normal_count=right_lighter_count,
                                     ),
                                 ))
     return frozenset(operations)
 
-#<<< class ScoredOperation(NamedTuple):
-#<<<     score: int
-#<<<     operation: Optional[ScaleOperation]
-#<<<
-#<<< def dump_args(func):
-#<<<     depth = 0
-#<<<     def wrapper(universes):
-#<<<         nonlocal depth
-#<<<
-#<<<         indent = "\t"*depth
-#<<<         invocation_str = f'{func.__qualname__}({pretty_print_universes(universes)})'
-#<<<         print(f'{indent}{invocation_str}')
-#<<<
-#<<<         depth += 1
-#<<<         ret_val = func(universes)
-#<<<         depth -= 1
-#<<<
-#<<<         print(f'{indent}{invocation_str} = {ret_val}')
-#<<<         return ret_val
-#<<<     return wrapper
-#<<<
-#<<< def get_worst_result_score(universes: Universes, operation: ScaleOperation) -> Optional[int]:
-#<<<     # Consider all the possible results of this operation, and figure out the worst cost.
-#<<<     worst_result_score = None
-#<<<     for result, next_universes in operate_scale(universes, operation).items():
-#<<<         if next_universes == universes:
-#<<<             # This operation gave us no new information, totally ignore it.
-#<<<             return None
-#<<<         score = search(next_universes).score + 1
-#<<<         if worst_result_score is None or score > worst_result_score:
-#<<<             worst_result_score = score
-#<<<
-#<<<     return worst_result_score
-#<<<
-#<<< memoed: Dict[Tuple, ScoredOperation] = {} #<<<
-#<<< #<<< @dump_args
-#<<< def search(universes: Universes) -> ScoredOperation:
-#<<<
-#<<<     if len(universes) == 1:
-#<<<         return ScoredOperation(0, None)
-#<<<     elif len(universes) == 2:
-#<<<         universe_1, universe_2 = list(universes)
-#<<<         if universe_1.islander == universe_2.islander:
-#<<<             return ScoredOperation(0, None)
-#<<<
-#<<<     analyzed_universes = analyze_universes(universes)
-#<<<     global memoed
-#<<<     result = memoed.get(analyzed_universes.key(), None)
-#<<<     if result is not None:
-#<<<         return result
-#<<<
-#<<<     best_scored_operation = None
-#<<<     for operation in get_operations(analyzed_universes):
-#<<<         worst_result_score = get_worst_result_score(universes, operation)
-#<<<         if worst_result_score is None:
-#<<<             continue
-#<<<         if best_scored_operation is None or worst_result_score < best_scored_operation.score:
-#<<<             best_scored_operation = ScoredOperation(worst_result_score, operation)
-#<<<
-#<<<     assert best_scored_operation is not None
-#<<<     memoed[analyzed_universes.key()] = best_scored_operation #<<<
-#<<<     return best_scored_operation
+@dataclass(frozen=True)
+class ScoredOperation:
+    """An operation with a score associated."""
+    score: int
+    operation: Optional[ScaleOperation]
+
+def get_worst_result_score(islanders: Islanders, operation: ScaleOperation) -> Optional[int]:
+    """Consider all the possible results of this operation, and figure out the worst cost."""
+    worst_result_score = None
+    for _result, next_islanders in operate_scale(operation).items():
+        if next_islanders == islanders:
+            # This operation gave us no new information, totally ignore it.
+            return None
+        score = search(next_islanders).score + 1
+        if worst_result_score is None or score > worst_result_score:
+            worst_result_score = score
+
+    return worst_result_score
+
+@functools.lru_cache(maxsize=None)
+def search(islanders: Islanders) -> ScoredOperation:
+    """Return the optimal operation to take given Islanders."""
+    if islanders.is_solved():
+        return ScoredOperation(0, None)
+
+    best_scored_operation = None
+    for operation in get_operations(islanders):
+        worst_result_score = get_worst_result_score(islanders, operation)
+        if worst_result_score is None:
+            continue
+        if best_scored_operation is None or worst_result_score < best_scored_operation.score:
+            best_scored_operation = ScoredOperation(worst_result_score, operation)
+
+    assert best_scored_operation is not None
+    return best_scored_operation
 
 ISLANDERS = Islanders(clueless_count=12)
 
@@ -272,12 +278,50 @@ def test():
     for result, islanders in islanders_by_result.items():
         print(f"\t{result}:\t{islanders}")
 
+class Node:
+    """TODO"""
+
+    def __init__(self, islanders: Islanders, parent: Optional['Node'] = None) -> None:
+        self.islanders = islanders
+        self.operation: Optional[ScaleOperation] = None
+        self.children: List['Node'] = []
+        if parent:
+            parent.children.append(self)
+
+    def __str__(self) -> str:
+        s = ""
+        s += str(self.islanders)
+        if self.operation is not None:
+            s += " now do " + str(self.operation)
+        return s
+
+def build_tree(original_islanders: Islanders) -> Node:
+    """TODO"""
+    root_node = Node(original_islanders)
+    fringe = [root_node]
+    next_fringe: List[Node] = []
+    while True:
+        if not fringe:
+            if not next_fringe:
+                break
+            fringe, next_fringe = next_fringe, fringe
+
+        node = fringe.pop()
+        operation = search(node.islanders).operation
+        if operation is not None:
+            node.operation = operation
+            for next_islanders in operate_scale(operation).values():
+                next_fringe.append(Node(next_islanders, parent=node))
+
+    return root_node
+
 def main():
     """Go go go!"""
 
-    test()
-    #<<< scored_operation = search(UNIVERSES)
-    #<<< print(scored_operation)
+    #<<< test()
+
+    tree = build_tree(ISLANDERS)
+    pptree.print_tree(tree)
 
 if __name__ == "__main__":
     main()
